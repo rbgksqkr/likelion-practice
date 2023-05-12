59ffadaeecd36722c978a98046c2fe3db02f84de
rbgksqkr <rbgks1937@naver.com>
2023-05-12 17:44:30 +0900

    Feat: get API로 sample data 가져오기

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const contentContainer = document.getElementById("content-container");
 const url = "http://likelion.kro.kr:8000/";
 
 const fetchPostList = async () => {
-  const data = await fetch(url)
+  const fetchList = await fetch(url)
     .then((res) => res.json())
     .catch((error) => console.log("data fetch error:", error));
+  const data = fetchList.data;
 
-  if (data.length === 0) {
+  if (data.length > 0) {
     data.map((item) => {
       const container = document.createElement("div");
       container.innerText = `작성자 : ${item.writer} / 내용 : ${item.content}`;

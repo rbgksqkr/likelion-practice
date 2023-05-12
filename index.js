b73b82e4373e4edf8046fe523d51cdc5ecad0915
rbgksqkr <rbgks1937@naver.com>
2023-05-12 13:38:35 +0900

    Feat: 홈화면 생성 및 get API 테스트

---
+++ index.js
@@ -0,0 +1,23 @@
+const contentContainer = document.getElementById("content-container");
+const url = "http://likelion.kro.kr:8000/";
+
+const fetchPostList = async () => {
+  const data = await fetch(url)
+    .then((res) => res.json())
+    .catch((error) => console.log("data fetch error:", error));
+
+  if (data.length === 0) {
+    data.map((item) => {
+      const container = document.createElement("div");
+      container.innerText = `작성자 : ${item.writer} / 내용 : ${item.content}`;
+      contentContainer.appendChild(container);
+    });
+  } else {
+    const container = document.createElement("div");
+    container.innerText =
+      "방명록에 글이 없습니다 \n\n 첫 방명록을 적어주세요 !";
+    contentContainer.appendChild(container);
+  }
+
+  console.log(data);
+};
